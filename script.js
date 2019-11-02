8e00257f2368feaf93a7ce375c308c6a69d5fe4c
Amos Ko <43330519+Bledwings@users.noreply.github.com>
2019-11-01 20:06:31 -0700

    Add files via upload
    v1

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("nav");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+</script>
