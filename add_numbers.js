ce4224179cb7dcd68f27e6dbae443641cb75a231
Astellarius <zak.lake0@gmail.com>
2020-04-24 01:15:51 -0700

    deleted unused files

--- add_numbers.js
+++
@@ -1,9 +0,0 @@
-function calculate_form() {
-
-    let first_num = Number(document.getElementById('first_num').value);
-    let second_num = Number(document.getElementById('second_num').value);
-
-    let total = first_num + second_num;
-
-    alert(total);
-}
--- gameController/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("gamepadconnected", function(e) {
-    var gp = navigator.getGamepads()[e.gamepad.index];
-    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-        gp.index, gp.id,
-        gp.buttons.length, gp.axes.length);
-});
-
