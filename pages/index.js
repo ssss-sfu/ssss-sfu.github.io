0598169d249b8bd9d0518b1d5ead15239156d6f1
Ben DJukastein <bendjukastein@gmail.com>
2022-08-30 17:38:47 -0700

    Inital commit for first version of the new Header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Button } from '@components/button'
+import Header from "../components/header.js";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <title>Software Systems Student Society</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <Header />
       <h1>Software Systems Student Society</h1>
       <Button label='Primary button'/>
       <Button label='Secondary button' type='secondary'/>
