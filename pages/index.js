8538626745059e5fdbe4f676ddcd2cc728d4f999
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-10-10 15:06:36 -0700

    Finish hero

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import { Helmet } from '@components'
+import HappySeb from '@images/seb/happy-seb-head.svg'
 
 export default function LandingPage() {
+
   return (
     <div className='landing-page'>
       <Helmet />
       <main className='hero'>
         <p>Welcome to the</p>
         <h1>Software Systems Student Society</h1>
+        <img src={HappySeb.src} alt='Happy Seb, the SSSS mascot' />
       </main>
     </div>
   )
