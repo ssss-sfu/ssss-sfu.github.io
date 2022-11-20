d438053b53437f8d7bf67f8f6879ab3631b8eecc
Marcus Chan <marcusgchan35@gmail.com>
2022-11-20 10:54:25 -0800

    ran prettier

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,3 +1,3 @@
-export const Button = ({label, type = 'primary'}) => {
-  return <button className={`btn ${type}`}>{label}</button>
-}
+export const Button = ({ label, type = "primary" }) => {
+  return <button className={`btn ${type}`}>{label}</button>;
+};
--- components/header.js
+++ components/header.js
@@ -1,40 +1,40 @@
-import Image from "next/image"
-import { Button } from '@components'
+import Image from "next/image";
+import { Button } from "@components";
 
-function Header() {    
-  return (        
-      <div class="header">
-        <div class="home-link">
-          <Image src="/sosy-logo.svg" height={36} width={36} />
-          <div>
-            <h6>Software Systems</h6>
-            <h5>Student Society</h5>
-          </div> 
-        </div> 
-
-        <div class="pages">
-          <a class="page-link">About</a>
-          <a class="page-link">Events</a>
-          <a class="page-link">Get Involved</a>
-          <a class="page-link">Resources</a>
-        </div>
-
-        <div class="socials">
-          <a href="https://www.facebook.com/ssss.sfu">
-            <Image src="/social-media/facebook.svg" height={18} width={18} />
-          </a>
-          <a  href="https://www.linkedin.com/company/ssss-sfu/">
-            <Image src="/social-media/linkedin.svg" height={18} width={18} />
-          </a>
-          <a href="https://www.instagram.com/ssss.sfu/">
-            <Image src="/social-media/instagram.svg" height={18} width={18} />
-          </a>
-          <a href="https://discord.gg/XZUd7amxPq">
-            <Image src="/social-media/discord.svg" height={18} width={18} />
-          </a>
-          <Button label='Contact Us'/>
+function Header() {
+  return (
+    <div class="header">
+      <div class="home-link">
+        <Image src="/sosy-logo.svg" height={36} width={36} />
+        <div>
+          <h6>Software Systems</h6>
+          <h5>Student Society</h5>
         </div>
       </div>
-  )
+
+      <div class="pages">
+        <a class="page-link">About</a>
+        <a class="page-link">Events</a>
+        <a class="page-link">Get Involved</a>
+        <a class="page-link">Resources</a>
+      </div>
+
+      <div class="socials">
+        <a href="https://www.facebook.com/ssss.sfu">
+          <Image src="/social-media/facebook.svg" height={18} width={18} />
+        </a>
+        <a href="https://www.linkedin.com/company/ssss-sfu/">
+          <Image src="/social-media/linkedin.svg" height={18} width={18} />
+        </a>
+        <a href="https://www.instagram.com/ssss.sfu/">
+          <Image src="/social-media/instagram.svg" height={18} width={18} />
+        </a>
+        <a href="https://discord.gg/XZUd7amxPq">
+          <Image src="/social-media/discord.svg" height={18} width={18} />
+        </a>
+        <Button label="Contact Us" />
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
