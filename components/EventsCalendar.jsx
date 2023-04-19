095ee6ec99ada0e2e50b32c2d99394c6181aa753
dannyl1u <dannyjialiliu@gmail.com>
2023-04-18 23:33:35 -0700

    use useState to prevent useEffect from running twice

--- components/EventsCalendar.jsx
+++ components/EventsCalendar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, dateFnsLocalizer} from 'react-big-calendar';
 import format from "date-fns/format";
 import getDay from "date-fns/getDay";
@@ -21,16 +21,17 @@ const localizer = dateFnsLocalizer({
 
 export default function EventsCalendar() {
   const calendarURL = 'https://clients6.google.com/calendar/v3/calendars/j7qfcngd9crbhelib6tgdihi3k@group.calendar.google.com/events?calendarId=j7qfcngd9crbhelib6tgdihi3k%40group.calendar.google.com&singleEvents=true&timeZone=America%2FVancouver&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2023-03-26T00%3A00%3A00-07%3A00&timeMax=2023-05-07T00%3A00%3A00-07%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs';
-  const events = [];
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    fetch(calendarURL).then((response) => {
-      response.json().then((data) => {
-        for (const event of data.items) {
-          events.push({title: event.summary, start: new Date(event.start.dateTime), end: new Date(event.end.dateTime)});
-        };
-        console.log(events);
-      });
+    fetch(calendarURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const updatedEvents = [];
+      for (const event of data.items) {
+        updatedEvents.push({title: event.summary, start: new Date(event.start.dateTime), end: new Date(event.end.dateTime)});
+      }
+      setEvents(updatedEvents);
     });
   }, []);
 
