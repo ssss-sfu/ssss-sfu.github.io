3eccaa561431cc89451a9a2fb2b69571affb3289
Risa Kawagoe <risa.kawagoe.dev@gmail.com>
2023-08-13 23:53:03 -0700

    added basic styling

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,12 @@
 import { Helmet, HeaderNav, Footer } from "@components";
-import { Dropdown } from "components/Dropdown.jsx";
+import Image from "next/image";
+import clock from "../public/images/blog-page/clock.svg";
+import person from "../public/images/blog-page/person.svg";
+import thumbnailPath from "../public/images/blog-page/hero-blog.jpg";
 
 export default function ResourcesPage() {
+  // let path = "../public/images/blog-page/hero-blog.jpg"
+
   return (
     <div className="blog-page">
       <Helmet />
@@ -11,8 +16,209 @@ export default function ResourcesPage() {
           <p>Blog</p>
           <h1>Learn more about Software Systems at SFU</h1>
         </header>
-        <article className="container">First container</article>
-        <article className="container">Second container</article>
+        <section className="container">
+          <h3 className="category-title">Featured</h3>
+          <div className="posts-list col-1">
+            <article className="post">
+              <div
+                className="thumbnail"
+                style={{ backgroundImage: `url(${thumbnailPath.src})` }}
+              ></div>
+              <div className="text">
+                <h4 className="title">Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+          </div>
+        </section>
+        <section className="container">
+          <h3 className="category-title">Alumni</h3>
+          <div className="posts-list col-3">
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+          </div>
+          <p className="see-more">
+            <button>See more</button>
+          </p>
+        </section>
+        <section className="container">
+          <h3 className="category-title">Co-op Experience</h3>
+          <div className="posts-list col-1">
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+          </div>
+          <p className="see-more">
+            <button>See more</button>
+          </p>
+        </section>
+        <section className="container">
+          <h3 className="category-title">Undergraduate Study</h3>
+          <div className="posts-list col-3">
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+            <article className="post">
+              <div className="thumbnail"></div>
+              <div className="text">
+                <h4>Title of the blog post</h4>
+                <div className="meta-info">
+                  <Image src={clock} height={18} width={18} alt="Clock" />
+                  <p>June 6th 2023</p>
+                  <Image src={person} height={18} width={18} alt="Person" />
+                  <p>First Last</p>
+                </div>
+                <p className="summary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </article>
+          </div>
+          <p className="see-more">
+            <button>See more</button>
+          </p>
+        </section>
       </main>
       <Footer />
     </div>
