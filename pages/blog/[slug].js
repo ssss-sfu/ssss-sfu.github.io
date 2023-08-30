01fa7339a3ea29b4109ec317565bdcc13828a578
CozySchmidt <t10.schmidt@gmail.com>
2023-08-29 22:31:32 -0700

    Added in Markdown library, still need to clean up code and implement slugs into frontend

---
+++ pages/blog/[slug].js
@@ -0,0 +1,75 @@
+import markdownconvert from "../../lib/markdownconversion";
+import { getPostBySlug, getAllPosts } from "../../lib/api";
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+
+export default function Post({ post, morePosts, preview }) {
+  const router = useRouter();
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>{title}</title>
+                <meta property="og:image" content={post.ogImage.url} />
+              </Head>
+              <PostHeader
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+                author={post.author}
+              />
+              <PostBody content={post.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const post = getPostBySlug(params.slug, [
+    "title",
+    "date",
+    "slug",
+    "author",
+    "content",
+    "coverImage",
+  ]);
+
+  const content = await markdownconvert(post.content || "");
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(["slug"]);
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
