0097b43ce4c6c350ebe3bb1caa575cbd6ec0966d
bra40 <brian_rahadi@sfu.ca>
2023-10-31 22:53:17 -0700

    Minimal ordering on posts

--- components/Card.jsx
+++ components/Card.jsx
@@ -7,29 +7,27 @@ import Link from "next/link";
 
 export default function Card({ post }) {
   return (
-    <div className="posts-list featured">
-      <div className="post">
-        <Link as={`/post/${post.slug.current}`} href="/post/[slug]">
-          <article className="post">
-            <div className="thumbnail">
-              <Image
-                src={urlForImage(post.mainImage).width(500).height(300).url()}
-                alt="thumbnail"
-                layout="fill"
-              />
-              <div className="overlay"></div>
+    <div className="post">
+      <Link as={`/post/${post.slug.current}`} href="/post/[slug]">
+        <article className="post">
+          <div className="thumbnail">
+            <Image
+              src={urlForImage(post.mainImage).width(500).height(300).url()}
+              alt="thumbnail"
+              layout="fill"
+            />
+            <div className="overlay"></div>
+          </div>
+          <div className="text">
+            <div className="title">{post.title}</div>
+            <div className="meta-info">
+              <Image src={clock} height={16} width={16} alt="Clock" />
+              <div>{formatDate(post._createdAt)}</div>
             </div>
-            <div className="text">
-              <div className="title">{post.title}</div>
-              <div className="meta-info">
-                <Image src={clock} height={16} width={16} alt="Clock" />
-                <div>{formatDate(post._createdAt)}</div>
-              </div>
-              <div className="summary">{post.excerpt}</div>
-            </div>
-          </article>
-        </Link>
-      </div>
+            <div className="summary">{post.excerpt}</div>
+          </div>
+        </article>
+      </Link>
     </div>
   );
 }
