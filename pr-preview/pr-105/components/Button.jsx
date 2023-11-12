a3a67de99ae8abc2ecf55d8f18164b9175e1c359
brianrahadi <brianrahadi@users.noreply.github.com>
2023-11-12 23:43:54 +0000

    Remove preview for PR 105 🛬

--- pr-preview/pr-105/components/Button.jsx
+++
@@ -1,3 +0,0 @@
-export const Button = ({ label, type = "primary" }) => {
-  return <button className={`btn ${type}`}>{label}</button>;
-};
--- pr-preview/pr-105/components/index.js
+++
@@ -1,6 +0,0 @@
-export { Button } from "./Button";
-export { Helmet } from "./Helmet";
-export { Hero } from "./Hero";
-export { HeaderNav } from "./HeaderNav";
-export { Logo } from "./Logo";
-export { Footer } from "./Footer";
