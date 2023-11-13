d231b8fc314b330740d13df94889314ebe966f49
bra40 <brian_rahadi@sfu.ca>
2023-11-13 15:14:15 -0800

    Refactor EventsCaledar to ts

--- components/EventsCalendar.tsx
+++ components/EventsCalendar.tsx
@@ -5,7 +5,7 @@ import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 
-const locales = {
+const locales: { [key: string]: any } = {
   "en-US": require("date-fns/locale/en-US"),
 };
 
@@ -17,16 +17,16 @@ const localizer = dateFnsLocalizer({
   locales,
 });
 
-export const EventsCalendar = () => {
-  const calendarURL =
+export const EventsCalendar: React.FC = () => {
+  const calendarURL: string =
     "https://clients6.google.com/calendar/v3/calendars/j7qfcngd9crbhelib6tgdihi3k@group.calendar.google.com/events?calendarId=j7qfcngd9crbhelib6tgdihi3k%40group.calendar.google.com&singleEvents=true&timeZone=America%2FVancouver&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2023-03-26T00%3A00%3A00-07%3A00&timeMax=2024-05-07T00%3A00%3A00-07%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs";
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState<any[]>([]);
 
   useEffect(() => {
     fetch(calendarURL)
       .then((response) => response.json())
       .then((data) => {
-        const updatedEvents = [];
+        const updatedEvents: any[] = [];
         for (const event of data.items) {
           updatedEvents.push({
             title: event.summary,
