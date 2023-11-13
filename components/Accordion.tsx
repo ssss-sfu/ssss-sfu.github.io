94fe00b8499ead5f499f547b006a67a42e6cdeaf
bra40 <brian_rahadi@sfu.ca>
2023-11-13 15:02:08 -0800

    Refactor Accordion to ts (missed component)

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,34 +1,57 @@
 import { useRef } from "react";
 
-export const Accordion = ({ data }) => {
-  const previouslyOpenedPanelIndex = useRef();
-  const panelsRef = useRef();
-  function getIdPanelMap() {
+interface AccordionProps {
+  data: { name: string; content: string[] }[];
+}
+
+const Accordion: React.FC<AccordionProps> = ({ data }) => {
+  const previouslyOpenedPanelIndex = useRef<number>(-1);
+  const panelsRef = useRef<Map<number, HTMLUListElement>>();
+
+  function getIdPanelMap(): Map<number, HTMLUListElement> {
     if (panelsRef.current) {
       return panelsRef.current;
     }
     panelsRef.current = new Map();
     return panelsRef.current;
   }
-  function toggleAccordian(indexClicked) {
+
+  function toggleAccordian(indexClicked: number): void {
     const idPanelMap = getIdPanelMap();
     const previouslyOpenedNode = idPanelMap.get(
       previouslyOpenedPanelIndex.current
     );
     const selectedNode = idPanelMap.get(indexClicked);
-    if (selectedNode.dataset.hasOwnProperty("opened")) {
+
+    if (selectedNode === undefined) {
+      return;
+    }
+
+    if (selectedNode?.dataset.hasOwnProperty("opened")) {
       closePanel(selectedNode);
     } else {
       openPanel(selectedNode);
       if (
         previouslyOpenedPanelIndex.current !== indexClicked &&
-        previouslyOpenedPanelIndex.current !== undefined
+        previouslyOpenedPanelIndex.current !== undefined &&
+        previouslyOpenedNode !== undefined
       ) {
         closePanel(previouslyOpenedNode);
       }
     }
     previouslyOpenedPanelIndex.current = indexClicked;
   }
+
+  function openPanel(node: HTMLUListElement): void {
+    node.style.maxHeight = `${node.scrollHeight}px`;
+    node.setAttribute("data-opened", "");
+  }
+
+  function closePanel(node: HTMLUListElement): void {
+    node.style.maxHeight = "0";
+    node.removeAttribute("data-opened");
+  }
+
   return (
     <ul className="accordion">
       {data.map(({ name, content }, i) => (
@@ -55,12 +78,4 @@ export const Accordion = ({ data }) => {
   );
 };
 
-function openPanel(node) {
-  node.style.maxHeight = `${node.scrollHeight}px`;
-  node.setAttribute("data-opened", "");
-}
-
-function closePanel(node) {
-  node.style.maxHeight = "0";
-  node.removeAttribute("data-opened");
-}
+export default Accordion;
