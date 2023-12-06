9d2b3700cf8d99b7e358211a0512e5d883982d64
bra40 <brian_rahadi@sfu.ca>
2023-12-05 23:36:47 -0800

    Add main content paragraph

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -26,8 +26,32 @@ const Courses: React.FC = () => {
         <section className="main-content">
           <h1>Courses at Software Systems</h1>
           <p>
-            Mandatory courses offered as part of the degree requirement of SFU
-            Software Systems.
+            Mandatory and semi-mandatory courses offered as part of the degree
+            requirement of SFU Software Systems.
+          </p>
+          <p>
+            Note: Not all the courses are mandatory. In the Systems or Software
+            Engineering requirements, students only need to take 3 or 2 out of
+            the listed courses as example.
+          </p>
+          <p>
+            Refer to Software Systems{" "}
+            <a
+              href="https://www.sfu.ca/students/calendar/2024/spring/programs/software-systems/major/bachelor-of-science.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              program calendar site
+            </a>{" "}
+            or{" "}
+            <a
+              href="https://www.sfu.ca/computing/current-students/undergraduate-students/for"
+              target="_blank"
+              rel="noreferrer"
+            >
+              forms site
+            </a>{" "}
+            for official resources.
           </p>
         </section>
         <section className="requirements-section">
