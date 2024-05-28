35aa8827805c6a01e6c528ff5694e2816ac8b4f1
bra40 <brian_rahadi@sfu.ca>
2024-05-27 23:08:21 -0700

    Add new execs

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,7 @@ const About: React.FC = () => {
 
   // TODO add validation and test cases for future PRs!
   useEffect(() => {
-    let json: string = JSON.stringify(execs.current);
+    let json: string = JSON.stringify(execs["2024-2025"]);
     let profiles: Profile[] = JSON.parse(json);
     setCurrentExecs(profiles);
   }, []);
