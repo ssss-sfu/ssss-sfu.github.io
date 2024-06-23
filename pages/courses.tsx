29bc95a0f89ad91034812554ab917415b60bea27
Brian Rahadi <brian.rahadi@gmail.com>
2024-06-23 00:06:41 -0700

    Update courses

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -53,7 +53,7 @@ const Courses: React.FC = () => {
             </a>{" "}
             for official resources.
           </p>
-          <p>Last updated: June 5, 2024</p>
+          <p>Last updated: June 23, 2024</p>
         </section>
         <section className="requirements-section">
           <div
